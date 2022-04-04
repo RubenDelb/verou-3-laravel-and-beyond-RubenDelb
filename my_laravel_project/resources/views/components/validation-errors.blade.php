d6cbdd3e7fe31348f1b413bf5da847799fa11c38
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-04 16:59:27 +0200

    Rewrite the x-auth-vali... to x-vali...

--- my_laravel_project/resources/views/components/validation-errors.blade.php
+++ my_laravel_project/resources/views/components/validation-errors.blade.php
@@ -1,10 +1,10 @@
 @if ($errors->any())
-    <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+    <div class="bg-red-100 mb-4 rounded">
+        <div class="font-medium text-red-600 px-2 pt-2">
             {{ __('Whoops! Something went wrong.') }}
         </div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+        <ul class="mt-3 list-disc list-inside text-sm text-red-600 p-2">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
