ad996f83f74f63a8a7943c1bdc18d8968661b7ae
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-05 12:09:16 +0200

    Create about-us page

---
+++ my_laravel_project/resources/views/about-us.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    {{-- <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('About Us') }}
+        </h2>
+    </x-slot> --}}
+
+    <div class="py-12">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden sm:rounded-lg">
+                <div class="p-6 text-xl sm:text-2xl">
+                    "Generosity could be as contagious as the zombie plague as long as enough people were willing to be carriers."
+                </div>
+                <div class="px-6 py-2 text-md">
+                    - Jonathan Maberry.
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
