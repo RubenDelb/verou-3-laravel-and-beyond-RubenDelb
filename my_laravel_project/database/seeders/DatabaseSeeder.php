1502510a9e3c1cf942527c5c6a505c815c71c1c4
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-05 15:40:18 +0200

    Populate database with test-data

--- my_laravel_project/database/seeders/DatabaseSeeder.php
+++ my_laravel_project/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tool;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        User::factory(20)->create();
+        Tool::factory(50)->create();
     }
 }
