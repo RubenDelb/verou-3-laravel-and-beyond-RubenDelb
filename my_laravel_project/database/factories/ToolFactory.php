0fd293513f0d535df4034e99d46a6bc520a7ac40
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-05 15:39:38 +0200

    Create tools table, populated with test-data

---
+++ my_laravel_project/database/factories/ToolFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tool>
+ */
+class ToolFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user-id' => random_int(1,20),
+            // 'tool' => $this->faker->text(),
+            'tool' => $this->faker->randomElement(['Hammer', 'Saw', 'Screwdriver']),
+            'province' => $this->faker->randomElement(['West-Vlaanderen', 'Oost-Vlaanderen', 'Antwerpen', 'Vlaams-Brabant', 'Limburg', 'Waals-Brabant', 'Henegouwen', 'Luik', 'Luxemburg', 'Namen']),
+            'available' => random_int(0,1),
+        ];
+    }
+}
---
+++ my_laravel_project/database/migrations/2022_04_05_122459_create_tools_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user-id');
+            $table->string('tool');
+            $table->string('province');
+            $table->boolean('available')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tools');
+    }
+};
