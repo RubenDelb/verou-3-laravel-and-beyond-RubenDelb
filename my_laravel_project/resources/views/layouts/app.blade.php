8f322506787cf4f217f399a12226318ca52e1d55
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-05 12:12:09 +0200

    Change bg to orange and add isset fucntion for possible future bugs

--- my_laravel_project/resources/views/layouts/app.blade.php
+++ my_laravel_project/resources/views/layouts/app.blade.php
@@ -17,20 +17,25 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-orange-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
+            @isset($header)
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
+            @endisset
+
 
             <!-- Page Content -->
+            @isset($slot)
             <main>
                 {{ $slot }}
             </main>
+            @endisset
         </div>
     </body>
 </html>
