a74c00ef8f59b28f5f69556b953ce534cf1f83c1
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-05 12:12:41 +0200

    Add route to about-us, and change homepage route

--- my_laravel_project/routes/web.php
+++ my_laravel_project/routes/web.php
@@ -15,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/','login');
+// Route::redirect('/','login');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about-us', function () {
+    return view('about-us');
+})->name('about-us');
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/dashboard', function () {
