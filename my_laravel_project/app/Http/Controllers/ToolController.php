2d0bbc11e81e0b3b8fed71b11ee458cdb01c4802
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-06 14:04:44 +0200

    Create ToolController to search wanted tool, and reserve a tool

---
+++ my_laravel_project/app/Http/Controllers/ToolController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tool;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ToolController extends Controller
+{
+    public function index(Request $request)
+    {
+        // $userTools = User::find(1)->allUserTools;
+
+        $tools = Tool::join('users', 'users.id', '=', 'tools.user_id')
+            ->select('tools.id', 'tool', 'province', 'name', 'email', 'user_id', 'available')
+            ->where([['tool', $request->tool], ['province', $request->province]])
+            ->get();
+
+        // dd($userTools);
+        return view('searched-tools', compact('tools'));
+    }
+
+    public function reserveTool(Request $request)
+    {
+        $wishedTool = Tool::join('users', 'users.id', '=', 'tools.user_id')
+            ->select('tools.id', 'tool', 'province', 'name', 'email', 'user_id')
+            ->where('tools.id', $request->toolId)
+            ->first();
+
+        // dd($wishedTool);
+        return view('reservation', compact('wishedTool'));
+    }
+}
