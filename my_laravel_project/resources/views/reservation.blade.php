b0033fbba62e4c71bc54969b35a064d834a240f2
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-06 14:08:12 +0200

    Create reservation view, to make a reservation definitive

---
+++ my_laravel_project/resources/views/reservation.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="mx-auto sm:px-6 lg:px-8 max-w-lg sm:w-full">
+            <div class="mt-4 text-center">
+                <div class="text-xl sm:text-3xl font-extrabold">
+                    Confirm your reservation.
+                </div>
+            </div>
+            <div class="text-center bg-red-50 underline p-6">
+                You want to reserve {{ $wishedTool->name }}'s {{ $wishedTool->tool }}?
+            </div>
+        </div>
+    </div>
+</x-app-layout>
