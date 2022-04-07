dc2a441df1215a36c8610e146edb1480f680af67
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-07 17:58:17 +0200

    Add a footer that extends on all pages

---
+++ my_laravel_project/resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-stone-300 text-sm text-center">
+    <div class="font-bold pt-6">Copyright © 2022 - Neighbourly</div>
+    <div class="px-8 pb-4">
+        Belgium's No. 1 Tool loaning platform | Site Map Use of this site constitutes acceptance of our User Agreement,
+        Privacy and Cookies policy
+        <br>
+        Legal. Contact us :
+        <a href="tel:+32 498 76 54 321">+32 498 76 54 321</a></div>
+</footer>
